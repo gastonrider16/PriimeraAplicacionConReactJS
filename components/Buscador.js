86e597a005c010d27d6a720810b757851009c763
gastonrider16 <gastonrider16@gmail.com>
2021-07-26 13:58:40 -0300

    Version 1 de components

---
+++ components/Buscador.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Buscador extends Component {
+    busquedaRef = React.createRef();
+    obtenerDatos = (e) => { //Metodo
+        e.preventDefault();
+        alert(this.busquedaRef.current.value);
+    }
+    render(){
+        return(
+            <form onSubmit={this.obtenerDatos}> {/*Invocacion submit */}
+                <div className="row">
+                    {this.props.mensaje}
+                    <div className="form-group col-md-7"> {/*De boostrap. Para que tome de las 8 columnas que vienen por default*/}
+                        <input ref={this.busquedaRef} type="text" className="form-control
+                        form-control-lg" placeholder="Busca tu imagen.
+                        Ejemplo: Futbol"/>
+                    </div>
+                    <div className="form-group col-md-4">
+                        <input type="submit" className="btn btn-lg btn-dange btn-block" 
+                        value="Buscar.."/>
+                        {/*<link rel="stylesheet" href="estiloBoton.css"/>*/}
+                        <style>{".btn { background-color: red; }"}</style>
+                    </div>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default Buscador;
