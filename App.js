fb223f29db9a5f334af9f536cf03bef46442f8f0
gastonrider16 <gastonrider16@gmail.com>
2021-07-26 14:53:39 -0300

    Version 2 App paso de ser de una FUNCION a una CLASE porque no me aceptaba metodo datosBusqueda

--- App.js
+++ App.js
@@ -1,16 +1,44 @@
+import react from 'react';
 import Buscador from './components/Buscador';
 
-function App() {
+class App extends react.Component {
+  datosBusqueda = (termino) => {
+    console.log(termino);
+  }
+
+  render(){
+    return (
+      <div className="app container">
+        <div className="jumbotron">
+          <p className="lead text-center">
+            Buscador de imágenes
+          </p>
+          <Buscador 
+            datosBusqueda={this.datosBusqueda}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+/*function App() {
+  datosBusqueda = (termino) => {
+    alert(termino);
+  }
+
   return (
     <div className="app container">
       <div className="jumbotron">
         <p className="lead text-center">
           Buscador de imágenes
         </p>
-        <Buscador mensaje="Buscador."/>
+        <Buscador 
+          datosBusqueda={this.datosBusqueda}
+        />
       </div>
     </div>
   );
-}
+}*/
 
 export default App;
