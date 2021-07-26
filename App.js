ddd8ac5c0e2a1c4f7ab4076838cf40ead5cca990
gastonrider16 <gastonrider16@gmail.com>
2021-07-25 22:45:18 -0300

    Version 1

---
+++ App.js
@@ -0,0 +1,16 @@
+import Buscador from './components/Buscador';
+
+function App() {
+  return (
+    <div className="app container">
+      <div className="jumbotron">
+        <p className="lead text-center">
+          Buscador de imágenes
+        </p>
+        <Buscador mensaje="Buscador.."/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
